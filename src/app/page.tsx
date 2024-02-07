cf793246e63f4b947699f66eef43566a0c52b4e8
Jocelyn Wensloff <jewensloff@gmail.com>
2024-02-07 15:30:42 -0700

    Remove the boiler plate code

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center">
+      Hello world
+      This is the landing page.
+    </main>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,12 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+      // screens: {
+      //   sm: '480px',
+      //   md: '768px',
+      //   lg: '976px',
+      //   xl: '1440px',
+      // },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
