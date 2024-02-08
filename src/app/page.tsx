9c4b20335a7a44267a416b6e06f74b3afc7732d6
Scotty Brown <scottybrown98@gmail.com>
2024-02-07 18:15:49 -0600

    Configure GraphQL

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,22 @@
+'use client'
 import Image from 'next/image';
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
 import ProductGrid from '../components/ProductGrid/ProductGrid';
+import { ApolloProvider } from '@apollo/client';
+import client from '../apolloClient'
 
 export default function Home() {
+
   return (
+    <ApolloProvider client={client}>
+
     <main className='flex flex-col items-center'>
       <Header />
       Hello world This is the landing page.
       <ProductGrid />
       <Footer />
     </main>
+    </ApolloProvider>
   );
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import { useQuery, useMutation, gql } from '@apollo/client';
+import { LOAD_PRODUCTS } from '../../GraphQL/Queries'
 
 function Header() {
+  const { loading, error, data } = useQuery(LOAD_PRODUCTS);
+
+  if (data) {console.log(data)}
+
   return (
     <header>
       This is the header
