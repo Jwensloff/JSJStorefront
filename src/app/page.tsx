59685bf3e15384e33987b0156ef1a9f4b5d6ce4b
Scotty Brown <scottybrown98@gmail.com>
2024-02-07 18:41:00 -0600

    Configure GraphQL-Apollo Client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
-import Image from 'next/image';
+'use client'
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
 import ProductGrid from '../components/ProductGrid/ProductGrid';
+import { ApolloProvider } from '@apollo/client';
+import client from '../apolloClient'
 
 export default function Home() {
   return (
+    <ApolloProvider client={client}>
     <main className="flex flex-col items-center">
       <Header />
       Hello world This is the landing page.
       <ProductGrid />
       <Footer />
     </main>
+    </ApolloProvider>
   );
 }
