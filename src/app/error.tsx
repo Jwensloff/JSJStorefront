2ceca1a73a1a39af815b1835040e55bb451c9896
Scotty Brown <scottybrown98@gmail.com>
2024-02-12 15:12:13 -0600

    Add header links/styling

---
+++ src/app/error.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+function ErrorPage() {
+
+    console.error(Error)
+
+return <div>This is the error page</div>
+}
+
+export default ErrorPage
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,24 +1,22 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     // screens: {
-    //   sm: '480px',
-    //   md: '768px',
-    //   lg: '976px',
-    //   xl: '1440px',
+    //   sm: "480px",
+    //   md: "768px",
+    //   lg: "976px",
+    //   xl: "1440px",
     // },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "gray-custom": "#D9D9D9",
       },
     },
   },
