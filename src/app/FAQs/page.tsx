e76397661b8c6daddfb5839ecc46d3e829b05188
Jocelyn Wensloff <jewensloff@gmail.com>
2024-02-11 20:01:38 -0700

    Create the Footer component

---
+++ src/app/FAQs/page.tsx
@@ -0,0 +1,3 @@
+export default function FAQs() {
+  return <div>FAQ page</div>;
+}
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,64 @@
-import React from 'react';
+import Link from 'next/link';
+
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faInstagram,
+  faTwitter,
+  faFacebook,
+} from '@fortawesome/free-brands-svg-icons';
 
 export default function Footer() {
-  return <div>This is the Footer</div>;
+  return (
+    <div className="flex justify-between text-center w-screen	p-2 px-10">
+      <div className="flex flex-col">
+        <h2 className="font-extrabold">Shop</h2>
+        <Link href="/womens" className="cursor-pointer hover:underline">
+          Women&apos;s
+        </Link>
+        <Link href="/womens" className="cursor-pointer hover:underline">
+          Men&apos;s
+        </Link>
+        <Link href="/womens" className="cursor-pointer hover:underline">
+          Jewelry
+        </Link>
+      </div>
+      <div className="flex flex-col">
+        <h2 className="font-extrabold">Company</h2>
+        <Link href="/careers" className="cursor-pointer hover:underline">
+          Careers
+        </Link>
+      </div>
+      <div className="flex flex-col">
+        <h2 className="font-extrabold">Help</h2>
+        <Link href="/order-status" className="cursor-pointer hover:underline">
+          Order Status
+        </Link>
+        <Link
+          href="/returns-&-exchanges"
+          className="cursor-pointer hover:underline"
+        >
+          Returns
+        </Link>
+        <Link href="/size-chart" className="cursor-pointer hover:underline">
+          Size Chart
+        </Link>
+        <Link href="/FAQs" className="cursor-pointer hover:underline">
+          FAQs
+        </Link>
+        <Link href="/contact" className="cursor-pointer hover:underline">
+          Contact Us
+        </Link>
+      </div>
+      <div className="flex flex-col	">
+        <h2 className="font-extrabold">Contact Us</h2>
+        <p className="cursor-pointer hover:underline"> +1-(800)-123-4567</p>
+        <div className="flex justify-evenly text-lg mt-1">
+          <FontAwesomeIcon icon={faInstagram} />
+          <FontAwesomeIcon icon={faTwitter} />
+          <FontAwesomeIcon icon={faFacebook} />
+        </div>
+      </div>
+    </div>
+  );
 }
