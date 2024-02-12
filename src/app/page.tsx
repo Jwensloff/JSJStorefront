72a6cac4b95c61ca5d5358324ce8e48d38a99668
Jocelyn Wensloff <jewensloff@gmail.com>
2024-02-12 15:20:15 -0700

    Create the Hero Image component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,14 @@ import HeroImage from "../components/HeroImage/HeroImage";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center">
+    <main className="flex flex-col items-center w-screen">
       <Header />
-      <HeroImage />
+      <HeroImage
+        text={"Here is some text"}
+        src={"/woman_jumping.jpg"}
+        alt={"A woman jumping while holding an umbrella"}
+        textLocation={""}
+      />
       Hello world This is the landing page.
       <ProductGrid />
       <Footer />
--- src/components/HeroImage/HeroImage.tsx
+++ src/components/HeroImage/HeroImage.tsx
@@ -1,15 +1,22 @@
-import Image from 'next/image';
+import Image from "next/image";
 
 interface HeroImageProps {
   text: string;
   src: string;
   alt: string;
+  textLocation: string;
 }
-export default function HeroImage({ text, src, alt }: HeroImageProps) {
+
+export default function HeroImage({
+  text,
+  src,
+  alt,
+  textLocation,
+}: HeroImageProps) {
   return (
-    <div>
-      This is the hero image
-      <Image src={src} alt={alt} fill />
+    <div className="h-[90vh] relative w-screen overflow-hidden">
+      <Image src={src} alt={alt} layout="fill" className="object-cover" />
+      <p className={`z-10 absolute ${textLocation}`}>{text}</p>
     </div>
   );
 }
