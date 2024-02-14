50681e530549aad0fff7e549a7016a2e1c53d982
Judy Ye <judyy.ye@gmail.com>
2024-02-14 13:04:20 -0800

    Remove tailwind styling and add style attribute along with minor styling changes

--- src/components/ProductGrid/ProductGrid.tsx
+++ src/components/ProductGrid/ProductGrid.tsx
@@ -14,29 +14,26 @@ export default function ProductGrid({ data, href }: ProductGridProps) {
   const [allProducts, setAllProducts] = useState<ProductTypes[]>(data);
 
   const products = allProducts?.map((product) => (
-    <Link
-      href={`/${href}/${product.id}`}
-      key={product.id}
-      className="flex-col max-w-80 max-h-96"
-    >
-      {/* I changed this from a div to a link so when a user clicks on a
-       product it will navigate to the individual product page */}
-
-      <div className="grid justify-center pb-4">
-        <Image
-          src={product.image}
-          width={170}
-          height={175}
-          alt={product.title}
-          className="object-contain max-h-full"
-        ></Image>
-      </div>
-      <h2 className="font-semibold">{product.title}</h2>
-      <p>${product.price}</p>
-      <p>
-        Rating: {product.rating.rate} <span>({product.rating.count})</span>
-      </p>
+    <Link href={`/${href}/${product.id}`} key={product.id}>
+      <section className="flex justify-center">
+        <div className="flex-col max-w-80 h-96">
+          <div className="grid justify-center pb-4">
+            <Image
+              src={product.image}
+              width={250}
+              height={250}
+              alt={product.title}
+              style={{ objectFit: "contain", height: "250px" }}
+            ></Image>
+          </div>
+          <h2 className="font-semibold line-clamp-2">{product.title}</h2>
+          <p>${product.price}</p>
+          <p>
+            Rating: {product.rating.rate} <span>({product.rating.count})</span>
+          </p>
+        </div>
+      </section>
     </Link>
   ));
-  return <div className="grid grid-cols-3 gap-24 p-5">{products}</div>;
+  return <div className="grid grid-cols-4 gap-8 p-5">{products}</div>;
 }
