8b633a4e863effc0451b01a8bf892dfa6b4c7161
Jocelyn Wensloff <jewensloff@gmail.com>
2024-02-14 08:57:22 -0700

    Create jewelry shop page

--- src/app/(shop)/jewelry/page.tsx
+++ src/app/(shop)/jewelry/page.tsx
@@ -1,10 +1,15 @@
+import Footer from "@/src/components/Footer/Footer";
+import Header from "@/src/components/Header/Header";
 import HeroImage from "@/src/components/HeroImage/HeroImage";
+import ProductGrid from "@/src/components/ProductGrid/ProductGrid";
 
 export default function page() {
   return (
     <div>
+      <Header />
       <HeroImage location={"jewelry"} />
-      This is the jewerly page
+      <ProductGrid />
+      <Footer />
     </div>
   );
 }
