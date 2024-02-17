11ce83ce0fd802a5c9c6f418cc83b7f5877ce23c
Scotty Brown <scottybrown98@gmail.com>
2024-02-16 20:18:12 -0600

    Wrapped entire page with <Suspense>

--- src/app/(shop)/search-results/page.tsx
+++ src/app/(shop)/search-results/page.tsx
@@ -1,31 +1,31 @@
-"use client";
-import Footer from "@/src/components/Footer/Footer";
-import Header from "@/src/components/Header/Header";
-import ProductGrid from "@/src/components/ProductGrid/ProductGrid";
-import { ProductTypes } from "@/src/types";
-import { useSearchParams } from "next/navigation";
-import { Suspense, useEffect, useState } from "react";
+'use client'
+import Footer from '@/src/components/Footer/Footer'
+import Header from '@/src/components/Header/Header'
+import ProductGrid from '@/src/components/ProductGrid/ProductGrid'
+import { ProductTypes } from '@/src/types'
+import { useSearchParams } from 'next/navigation'
+import { Suspense, useEffect, useState } from 'react'
 
 export default function SearchResults() {
-  const [allProducts, setAllProducts] = useState<ProductTypes[]>();
-  const searchParams = useSearchParams();
-  const search = searchParams.get("search");
+  const [allProducts, setAllProducts] = useState<ProductTypes[]>()
+  const searchParams = useSearchParams()
+  const search = searchParams?.get('search')
 
   useEffect(() => {
     const getUpdatedProductList = () => {
-      if (typeof window !== "undefined") {
-        let localStorage = window.localStorage;
-        let productArray = localStorage.getItem("allProductsArray");
+      if (typeof window !== 'undefined') {
+        let localStorage = window.localStorage
+        let productArray = localStorage.getItem('allProductsArray')
 
         if (productArray) {
-          return JSON.parse(productArray);
+          return JSON.parse(productArray)
         }
       }
-    };
+    }
 
-    const updatedProducts = getUpdatedProductList();
-    setAllProducts(updatedProducts);
-  }, []);
+    const updatedProducts = getUpdatedProductList()
+    setAllProducts(updatedProducts)
+  }, [])
 
   const searchedProducts = allProducts?.filter((product) => {
     return (
@@ -33,21 +33,21 @@ export default function SearchResults() {
       (product.title || product.description || product.category)
         .toLowerCase()
         .includes(search.toLowerCase())
-    );
-  });
+    )
+  })
 
   return (
     <>
       <Header />
-      {searchedProducts?.length === 0 && (
-        <p className="px-2">
-          Sorry, no products match your search. Please try something else!
-        </p>
-      )}
       <Suspense fallback={<div>Loading...</div>}>
+        {searchedProducts?.length === 0 && (
+          <p className="px-2">
+            Sorry, no products match your search. Please try something else!
+          </p>
+        )}
         {searchedProducts && <ProductGrid data={searchedProducts} />}
       </Suspense>
       <Footer />
     </>
-  );
+  )
 }
