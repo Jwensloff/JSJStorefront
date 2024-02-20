26678bf6edefd476c51720685a12b962781233ad
Scotty Brown <scottybrown98@gmail.com>
2024-02-19 19:49:24 -0600

    Add fetch req. and product detail jsx

--- src/app/(shop)/product/[id]/page.tsx
+++ src/app/(shop)/product/[id]/page.tsx
@@ -1,5 +1,8 @@
-const getProductData = async (id: number) => {
-  const response = await fetch(`https://fakestoreapi.com/products/${id}`);
+import Footer from "@/src/components/Footer/Footer";
+import Header from "@/src/components/Header/Header";
+
+const getLandingProductData = async (productID: string | undefined) => {
+  const response = await fetch(`https://fakestoreapi.com/products/${productID}`);
 
   if (!response.ok) {
     throw new Error("Oops, something went wrong");
@@ -8,9 +11,33 @@ const getProductData = async (id: number) => {
   return response.json();
 };
 
-export default async function page({ params }: { params: { id: number } }) {
-  const { id } = params;
-  const product = await getProductData(id);
+export default async function Product({ params }: any) {
+  const {id, title, price, description, image, rating} = await getLandingProductData(params.id);
 
-  return <div>{product?.title}</div>;
-}
+  const formattedDescription = description.split(/,|\//)
+  console.log(formattedDescription);
+
+  const productDisplay = (
+    <div className="flex ">
+      <img src={image} alt={title} />
+      <div className="flex-col">
+      <h2 className="text-xl font-bold">{title}</h2>
+      <ul>
+        {formattedDescription.map((item: string) => (
+          <li className="list-disc list-inside">{item}</li>
+          ))}
+      </ul>
+      <p>{price}</p>
+      <p>{rating.rate}</p>
+          </div>
+    </div>
+  )
+
+  return (
+    <div>
+      <Header />
+      <div>{productDisplay}</div>
+      <Footer />
+    </div>
+  )
+} 
