91ce377fc21d5b7873cb3dd5c322618c8b4763d0
Scotty Brown <scottybrown98@gmail.com>
2024-02-20 15:21:05 -0600

    Switch img tag to Nextjs Image tag

--- src/app/(shop)/product/[id]/page.tsx
+++ src/app/(shop)/product/[id]/page.tsx
@@ -1,6 +1,7 @@
 import Footer from '@/src/components/Footer/Footer'
 import Header from '@/src/components/Header/Header'
 import { Rating, Select, Option, Button } from '@/src/tailwind'
+import Image from 'next/image'
 
 const getLandingProductData = async (productID: string | undefined) => {
   const response = await fetch(`https://fakestoreapi.com/products/${productID}`)
@@ -25,7 +26,7 @@ export default async function Product({ params }: any) {
 
   const productDisplay = (
     <div className="lg:flex lg:flex-row sm:flex sm:flex-col sm:items-center  gap-40 m-10 w-fit">
-      <img src={image} alt={title} className="object-contain w-80 " />
+        <Image width={320} height={320} src={image} alt={title} />
       <div className="flex-col space-y-10 w-auto">
         <h2 className="text-4xl font-bold">{title}</h2>
         <div className="flex">
@@ -51,16 +52,20 @@ export default async function Product({ params }: any) {
         <div className="border border-black w-full m-1"></div>
         <div className="space-y-10">
           {category !== 'jewelery' && (
-          <Select variant="static" label="Select Size" placeholder={undefined}>
-            <Option value="xx-small">XX-Small</Option>
-            <Option value="x-small">X-Small</Option>
-            <Option value="small">Small</Option>
-            <Option value="medium">Medium</Option>
-            <Option value="large">Large</Option>
-            <Option value="x-large">X-Large</Option>
-            <Option value="xx-large">XX-Large</Option>
-            <Option value="xxx-large">XXX-Large</Option>
-          </Select>
+            <Select
+              variant="static"
+              label="Select Size"
+              placeholder={undefined}
+            >
+              <Option value="xx-small">XX-Small</Option>
+              <Option value="x-small">X-Small</Option>
+              <Option value="small">Small</Option>
+              <Option value="medium">Medium</Option>
+              <Option value="large">Large</Option>
+              <Option value="x-large">X-Large</Option>
+              <Option value="xx-large">XX-Large</Option>
+              <Option value="xxx-large">XXX-Large</Option>
+            </Select>
           )}
           <Select variant="static" label="Quantity" placeholder={undefined}>
             <Option value="1">1</Option>
@@ -70,12 +75,21 @@ export default async function Product({ params }: any) {
             <Option value="5">5</Option>
           </Select>
         </div>
-        <div className='space-x-5'>
-          <a href='/shopping-cart' >
-          <Button size='lg' color='blue' placeholder={undefined}>Add To Cart</Button>
+        <div className="space-x-5">
+          <a href="/shopping-cart">
+            <Button size="lg" color="blue" placeholder={undefined}>
+              Add To Cart
+            </Button>
           </a>
-          <a >
-          <Button size='lg' color='gray' variant='outlined'  placeholder={undefined}>Continue Shopping</Button>
+          <a>
+            <Button
+              size="lg"
+              color="gray"
+              variant="outlined"
+              placeholder={undefined}
+            >
+              Continue Shopping
+            </Button>
           </a>
         </div>
         <ul>
