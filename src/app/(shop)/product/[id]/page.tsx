72bf570bb489780fd3845353ca5caa95e05f2c9f
Scotty Brown <scottybrown98@gmail.com>
2024-02-20 23:01:30 -0600

    Update screen size for responsiveness

--- src/app/(shop)/product/[id]/page.tsx
+++ src/app/(shop)/product/[id]/page.tsx
@@ -15,7 +15,7 @@ const getLandingProductData = async (productID: string | undefined) => {
   return response.json();
 };
 
-export default async function Product({ params }: any) {
+export default async function Product({ params }: { params: { id: string } }) {
   const { id, title, price, description, image, rating, category } =
     await getLandingProductData(params.id);
   const formattedDescription = description.split(/,|\/|\./).filter(Boolean);
@@ -27,9 +27,9 @@ export default async function Product({ params }: any) {
   const roundedRating = roundRating(rating.rate);
 
   const productDisplay = (
-    <div className="lg:flex lg:flex-row sm:flex sm:flex-col sm:items-center  gap-40 m-10 w-fit">
-      <Image width={320} height={320} src={image} alt={title} />
-      <div className="flex-col space-y-10 w-auto">
+    <div className="lg:flex lg:flex-row sm:flex sm:flex-col sm:items-center  lg:gap-80 sm:gap-20 m-10 w-fit">
+      <Image width={320} height={320} src={image} alt={title} className="lg:ml-40" />
+      <div className="flex-col space-y-10 max-w-2xl">
         <h2 className="text-4xl font-bold">{title}</h2>
         <div className="flex">
           <div className="flex flex-row items-center gap-1 ">
