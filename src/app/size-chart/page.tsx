ba0fe1ef82bd13ec8164e60a396a981218f80189
Jocelyn Wensloff <jewensloff@gmail.com>
2024-02-21 15:10:38 -0700

    Add size charts

--- src/app/size-chart/page.tsx
+++ src/app/size-chart/page.tsx
@@ -1,3 +1,28 @@
+import Footer from "@/src/components/Footer/Footer";
+import Header from "@/src/components/Header/Header";
+import Image from "next/image";
+
 export default function page() {
-  return <div>size chart</div>;
+  return (
+    <div className="min-h-screen flex flex-col justify-between items-center">
+      <Header />
+      <div className="relative w-[90%] h-[70vh]">
+        <Image
+          src="/sizechart_women.jpg"
+          alt="womens size chart"
+          fill
+          objectFit="contain"
+        />
+      </div>
+      <div className="relative w-[90%] h-[70vh]">
+        <Image
+          src="/sizechart_men.jpg"
+          alt="womens size chart"
+          fill
+          objectFit="contain"
+        />
+      </div>
+      <Footer />
+    </div>
+  );
 }
