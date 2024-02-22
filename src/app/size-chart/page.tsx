d5556363e41727e7c01592e9921714f2446fc413
Judy Ye <judyy.ye@gmail.com>
2024-02-21 16:08:23 -0800

    Add responsiveness to size-chart page

--- src/app/size-chart/page.tsx
+++ src/app/size-chart/page.tsx
@@ -6,21 +6,23 @@ export default function page() {
   return (
     <div className="min-h-screen flex flex-col justify-between items-center">
       <Header />
-      <div className="relative w-[90%] h-[30vh] md:h-[50vh]">
-        <Image
-          src="/sizechart_women.jpg"
-          alt="womens size chart"
-          fill
-          objectFit="contain"
-        />
-      </div>
-      <div className="relative w-[90%] h-[30vh] md:h-[50vh]">
-        <Image
-          src="/sizechart_men.jpg"
-          alt="womens size chart"
-          fill
-          objectFit="contain"
-        />
+      <div>
+        <div className="relative w-[95vw] h-[50vh] md:h-[90vh]">
+          <Image
+            src="/sizechart_women.jpg"
+            alt="womens size chart"
+            fill
+            objectFit="contain"
+          />
+        </div>
+        <div className="relative w-[95vw] h-[50vh] md:h-[90vh]">
+          <Image
+            src="/sizechart_men.jpg"
+            alt="womens size chart"
+            fill
+            objectFit="contain"
+          />
+        </div>
       </div>
       <Footer />
     </div>
