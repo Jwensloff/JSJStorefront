c8fcb5d1db2e89009a3db3336f2c04ae434eb69c
Scotty Brown <scottybrown98@gmail.com>
2024-02-25 17:34:58 -0600

    Update error handing in supabase config

--- src/config/supabaseClient.ts
+++ src/config/supabaseClient.ts
@@ -1,6 +1,7 @@
 // Supabase client configuration
 
 import { createClient } from "@supabase/supabase-js";
+import { error } from "console";
 
 declare const NEXT_PUBLIC_SUPABASE_URL: string;
 declare const NEXT_PUBLIC_SUPABASE_ANON_KEY: string;
@@ -9,7 +10,7 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 
 if (!supabaseUrl || !supabaseKey) {
-  throw new Error("Supabase URL or key is not provided");
+  throw error;
 }
 
 const supabase = createClient(supabaseUrl, supabaseKey);
