78d7aa9be286135461a3419bc35e4542da401c6a
Scotty Brown <scottybrown98@gmail.com>
2024-02-25 17:17:26 -0600

    Run Lint Fix

--- src/config/supabaseClient.ts
+++ src/config/supabaseClient.ts
@@ -5,8 +5,8 @@ import { createClient } from "@supabase/supabase-js";
 declare const NEXT_PUBLIC_SUPABASE_URL: string | undefined;
 declare const NEXT_PUBLIC_SUPABASE_ANON_KEY: string | undefined;
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 
 if (!supabaseUrl || !supabaseKey) {
   throw new Error("Supabase URL or key is not provided");
