fa4290f407bb68e50553ee099845885785d595ef
Scotty Brown <scottybrown98@gmail.com>
2024-02-25 17:36:55 -0600

    Please lord

--- src/config/supabaseClient.ts
+++ src/config/supabaseClient.ts
@@ -9,10 +9,6 @@ declare const NEXT_PUBLIC_SUPABASE_ANON_KEY: string;
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 
-if (!supabaseUrl || !supabaseKey) {
-  throw error;
-}
-
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
