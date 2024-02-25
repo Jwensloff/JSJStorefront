391e78fc65281a40ff86556588dcb705eb528039
Scotty Brown <scottybrown98@gmail.com>
2024-02-25 17:39:55 -0600

    Delete declarations in supabaseClient

--- src/config/supabaseClient.ts
+++ src/config/supabaseClient.ts
@@ -1,10 +1,6 @@
 // Supabase client configuration
 
 import { createClient } from "@supabase/supabase-js";
-import { error } from "console";
-
-declare const NEXT_PUBLIC_SUPABASE_URL: string;
-declare const NEXT_PUBLIC_SUPABASE_ANON_KEY: string;
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
