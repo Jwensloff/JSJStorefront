7ddb969ac3f386b35f81f12297c92a316d3f6601
Scotty Brown <scottybrown98@gmail.com>
2024-02-25 17:19:57 -0600

    Update supabase variable types

--- src/config/supabaseClient.ts
+++ src/config/supabaseClient.ts
@@ -2,8 +2,8 @@
 
 import { createClient } from "@supabase/supabase-js";
 
-declare const NEXT_PUBLIC_SUPABASE_URL: string | undefined;
-declare const NEXT_PUBLIC_SUPABASE_ANON_KEY: string | undefined;
+declare const NEXT_PUBLIC_SUPABASE_URL: string;
+declare const NEXT_PUBLIC_SUPABASE_ANON_KEY: string;
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
