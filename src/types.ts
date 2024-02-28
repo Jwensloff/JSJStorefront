1ee87822e80a1198030e7c1934c210dd4206b04f
Scotty Brown <scottybrown98@gmail.com>
2024-02-28 16:31:40 -0600

    Add types for Jobs

--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,34 @@
 export interface ProductTypes {
-  id: number;
-  title: string;
-  price: number;
-  description: string;
-  category: string;
-  image: string;
+  id: number
+  title: string
+  price: number
+  description: string
+  category: string
+  image: string
   rate: {
-    rating: number;
-    count: number;
-  };
+    rating: number
+    count: number
+  }
+}
+
+export interface CareerProps {
+  id: number
+  created_at: Number
+  department: string
+  title: string
+  description: string
+  location: {
+    state: string
+    country: string
+    remote: boolean
+  }
+  salary: number
+  type: string
+  requirements: string
+  responsibilities: string
+  benefits: string
+  start_date: {
+    month: string
+    year: number
+  }
 }
