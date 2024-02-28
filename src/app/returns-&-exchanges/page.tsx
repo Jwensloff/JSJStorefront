7a96663e70274c811231e51b70b9bd35fc9a575e
Judy Ye <judyy.ye@gmail.com>
2024-02-28 14:00:24 -0800

    Add return and exchanges component and page content

--- src/app/returns-&-exchanges/page.tsx
+++ src/app/returns-&-exchanges/page.tsx
@@ -1,3 +1,13 @@
+import Footer from "@/src/components/Footer/Footer";
+import Header from "@/src/components/Header/Header";
+import ReturnsAndExchanges from "@/src/components/ReturnsAndExchanges/ReturnsAndExchanges";
+
 export default function page() {
-  return <div>returns and exchanges</div>;
+  return (
+    <div className="min-h-screen flex flex-col justify-between ">
+      <Header />
+      <ReturnsAndExchanges />
+      <Footer />
+    </div>
+  );
 }
---
+++ src/components/ReturnsAndExchanges/ReturnsAndExchanges.tsx
@@ -0,0 +1,37 @@
+import { faBoxOpen, faGift } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+
+export default function ReturnsAndExchanges() {
+  return (
+    <>
+      <h1 className="text-center text-lg sm:text-2xl font-bold mb-2">
+        Return and Exchanges
+      </h1>
+      <div className="flex flex-col sm:flex-row items-center gap-1 sm:justify-around">
+        <Link href={"/fallback"}>
+          <section className="flex flex-col h-auto justify-around max-w-56 sm:h-52  rounded border border-gray-500 text-center">
+            <FontAwesomeIcon
+              aria-hidden="true"
+              icon={faBoxOpen}
+              className="text-3xl pt-3"
+            />
+            <h2 className="font-bold">Start a Return</h2>
+            <p>Start a return today for any items you purchased from JSJ.</p>
+          </section>
+        </Link>
+        <Link href={"/fallback"}>
+          <section className="flex flex-col h-auto justify-around max-w-56 sm:h-52 rounded border border-gray-500 text-center">
+            <FontAwesomeIcon
+              aria-hidden="true"
+              icon={faGift}
+              className="text-3xl pt-3"
+            />
+            <h2 className="font-bold">Gift Returns</h2>
+            <p>Returning a gift? Our return process is quick and easy.</p>
+          </section>
+        </Link>
+      </div>
+    </>
+  );
+}
