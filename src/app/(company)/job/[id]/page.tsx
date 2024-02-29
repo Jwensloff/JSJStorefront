0b394f0b63a9724abca705bc974e1ada841118cd
Scotty Brown <scottybrown98@gmail.com>
2024-02-29 00:10:38 -0600

    Add id params for JobDetail use cases

--- src/app/(company)/job/[id]/page.tsx
+++ src/app/(company)/job/[id]/page.tsx
@@ -2,18 +2,23 @@ import supabase from "@/src/config/supabaseClient";
 import Footer from "../../../../components/Footer/Footer";
 import Header from "../../../../components/Header/Header";
 
-const getJobDetails = async () => {
-  let { data, error } = await supabase.from("open_jobs").select("*");
+const getJobDetails = async (id: number) => {
+  let { data, error } = await supabase.from("open_jobs").select('')
+  .eq("id", `${id}`)
 
   if (error) {
-    throw error;
+    throw error.message;
   }
   if (data) {
+    console.log(data);
     return data;
   }
 };
 
-export default async function JobDetails() {
+export default async function JobDetails({ params }: { params: { id: string } }) {
+
+const jobDetails = await getJobDetails(2);
+
   return (
     <div>
       <Header />
