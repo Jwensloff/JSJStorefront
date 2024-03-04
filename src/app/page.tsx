8316a95d7c634ad9cb3834fc5717de6e688ab716
Judy Ye <judyy.ye@gmail.com>
2024-03-03 17:46:43 -0800

    Switch previous createClient function to newly added createClient from supabase server on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,19 @@
-import Header from "../components/Header/Header";
-import Footer from "../components/Footer/Footer";
 import { Landing } from "../components/Landing/Landing";
-import supabase from "../utils/supabase/supabaseClient";
+
 import HeroImage from "../components/HeroImage/HeroImage";
-
-// fetch all product data
-const getLandingProductData = async () => {
-  // "use server";
-  let { data, error } = await supabase.from("products").select("*");
-
-  if (error) {
-    throw error;
-  }
-  if (data) {
-    return data;
-  }
-};
+import { createClient } from "../utils/supabase/supabaseServer";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
-  const products = await getLandingProductData();
-
+  const supabase = createClient();
+  const { data, error } = await supabase.from("products").select("*");
+  if (error) {
+    redirect("/error");
+  }
   return (
     <main className="flex flex-col items-center">
       <HeroImage location={"landing"} />
-      <Landing products={products} />
+      <Landing products={data} />
     </main>
   );
 }
