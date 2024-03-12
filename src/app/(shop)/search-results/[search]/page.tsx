7c071dc60aefd31345399fb80a2dffc66c71953e
Judy Ye <judyy.ye@gmail.com>
2024-03-12 13:56:18 -0700

    Add keyboard event to search and add explicit searched results to search-results page

--- src/app/(shop)/search-results/[search]/page.tsx
+++ src/app/(shop)/search-results/[search]/page.tsx
@@ -1,5 +1,3 @@
-import Footer from "@/src/components/Footer/Footer";
-import Header from "@/src/components/Header/Header";
 import ProductGrid from "@/src/components/ProductGrid/ProductGrid";
 import { ProductTypes } from "@/src/types";
 import { createClient } from "@/src/utils/supabase/supabaseServer";
@@ -29,15 +27,21 @@ export default async function SearchResults({
   });
 
   return (
-    <div className="min-h-screen flex flex-col justify-between">
-      {searchedProducts?.length === 0 && (
-        <div className="flex justify-center items-center">
-          <p data-test="search-error" className="px-2">
-            Sorry, no products match your search. Please try something else!
-          </p>
-        </div>
-      )}
-      {searchedProducts && <ProductGrid data={searchedProducts} />}
-    </div>
+    <>
+      <p className="pl-3 py-5 text-lg font-semibold">
+        Search Results for: &quot;{params.search}&quot;
+      </p>
+      <div className="min-h-screen flex flex-col justify-between">
+        {searchedProducts?.length === 0 && (
+          <div className="flex justify-center items-center">
+            <p data-test="search-error" className="px-2">
+              Sorry, no products match your search. Please try something else!
+            </p>
+          </div>
+        )}
+
+        {searchedProducts && <ProductGrid data={searchedProducts} />}
+      </div>
+    </>
   );
 }
