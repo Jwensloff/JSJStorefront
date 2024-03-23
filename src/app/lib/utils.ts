ada50909ca70a618f0aa67cdb81562e0ee5949c2
Scotty Brown <scottybrown98@gmail.com>
2024-03-23 13:18:08 -0500

    Add util functions for Careers

--- src/app/lib/utils.ts
+++ src/app/lib/utils.ts
@@ -1,5 +1,7 @@
-import { ProductTypes } from "@/src/types";
+import { ProductTypes, CareerProps } from "@/src/types";
 
+
+// Single Product Page Utilities
 export const roundRating = (num: number) => {
   return Math.round(num);
 };
@@ -7,3 +9,16 @@ export const roundRating = (num: number) => {
 export const formatDescription = (product: ProductTypes) => {
   return product?.description.split(/,|\/|\./).filter(Boolean);
 };
+
+// Single Job Page Utilities
+export const formatJobQualifications = (jobListing: CareerProps) => {
+    return jobListing?.qualifications.split(/\//).filter(Boolean);
+}
+
+export const formatJobResponsibilites = (jobListing: CareerProps) => {
+    return jobListing?.responsibilities.split(/\//).filter(Boolean);
+}
+
+export const formatJobBenefits = (jobListing: CareerProps) => {
+    return jobListing?.benefits.split(/,/).filter(Boolean);
+}
